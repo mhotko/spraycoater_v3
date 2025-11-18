c42c3764aa2d8d09cc56ffe815759b6feb451de1
ECatGroup <ecatgroup105@gmail.com>
2025-11-18 13:32:11 +0100

    on drag end does not need to set end point

--- src/controller/canvas_controller.py
+++ src/controller/canvas_controller.py
@@ -19,7 +19,8 @@ class CCanvas():
         self.model.set_end_point(Point(x, y))
 
     def _on_canvas_release(self, x: int, y: int):
-        self.model.set_end_point(Point(x, y))
+        # self.model.set_end_point(Point(x, y))
+        pass
     
     def _update_canvas(self):
         self.model.read_frame()
