f1a6f45b7f4f3b967d85f730fdcd53a4691101dc
mhotko <miha.hotko@gmail.com>
2025-11-19 19:49:00 +0100

    added serial manager base class

---
+++ src/util/serial_managers/serial_manager_base.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from serial.tools import list_ports
+
+
+class SerialManager(ABC):
+    def __init__(self, serial_number: str):
+        super().__init__()
+        self._serial_number = serial_number
+
+    @property
+    @abstractmethod
+    def is_connected(self):
+        pass
+
+    def seek_comport(self) -> str:
+        ports = list(list_ports.comports())
+        for p in ports:
+            if p.serial_number == self._serial_number:
+                return p.device
+        else:
+            return ""
