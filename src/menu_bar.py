3d3041971cdaea7aec95f381e0f9df0a8d621f1c
ECatGroup <ecatgroup105@gmail.com>
2025-11-20 10:55:52 +0100

    eventbus in menubar

--- src/menu_bar.py
+++ src/menu_bar.py
@@ -1,30 +1,15 @@
-from enum import Enum, auto
 import tkinter as tk
-from typing import Callable
 
-class MenuItemEnum(Enum):
-    CASCADE = auto()
-    MOVE_Y_220 = auto()
-    RESET_POSITION = auto()
-    CENTER_X = auto()
-    CENTER_Y = auto()
-    CENTER_XY = auto()
-    HOME_X = auto()
-    HOME_Y = auto()
-    HOME_Z = auto()
-    HOME_XYZ = auto()
-    T_100 = auto()
-    T_75 = auto()
-    T_50 = auto()
-    T_25 = auto()
-    T_0 = auto()
-    OPEN_SETTINGS = auto()
+from util.event_bus_service import event_bus
+from util.event_type_enum import EventEnum
+
 
 class MenuBar(tk.Menu):
-    def __init__(self, master: tk.Tk, on_event: Callable[[MenuItemEnum], None], **kwargs):
+    def __init__(self, master: tk.Tk, **kwargs):
         super().__init__(master, **kwargs)
 
-        self._on_event: Callable[[MenuItemEnum], None] = on_event
+        self.event_bus = event_bus
+        self.event_bus.register(self)
 
         self._calibrate_parent_item: tk.Menu = tk.Menu(self, tearoff=0)
         self._gantry_parent_item: tk.Menu = tk.Menu(self, tearoff=0)
@@ -37,12 +22,16 @@ class MenuBar(tk.Menu):
     def _create_calibrate_menu(self) -> None:
         self.add_cascade(label="Calibrate", menu=self._calibrate_parent_item)
         self._calibrate_parent_item.add_command(
-            label="Calibrate", command=lambda: self._on_event(MenuItemEnum.CASCADE))
+            label="Calibrate",
+            command=lambda: self.event_bus.publish(EventEnum.CASCADE),
+        )
 
     def _create_gantry_menu(self) -> None:
         self.add_cascade(label="Gantry", menu=self._gantry_parent_item)
         self._gantry_parent_item.add_command(
-            label="Move Y=220", command=lambda: self._on_event(MenuItemEnum.MOVE_Y_220))
+            label="Move Y=220",
+            command=lambda: self.event_bus.publish(EventEnum.MOVE_Y_220),
+        )
 
         self._gantry_parent_item.add_separator()
         self._create_gantry_move_menu()
@@ -50,49 +39,84 @@ class MenuBar(tk.Menu):
         self._create_gantry_temp_menu()
 
     def _create_gantry_move_menu(self) -> None:
-
-        gantry_move_menu: tk.Menu = tk.Menu(self._gantry_parent_item, tearoff=0)
+        gantry_move_menu: tk.Menu = tk.Menu(
+            self._gantry_parent_item, tearoff=0
+        )
         self._gantry_parent_item.add_cascade(
-            label="Move", menu=gantry_move_menu)
+            label="Move", menu=gantry_move_menu
+        )
         gantry_move_menu.add_command(
-            label="Reset Position", command=lambda: self._on_event(MenuItemEnum.RESET_POSITION))
+            label="Reset Position",
+            command=lambda: self.event_bus.publish(EventEnum.RESET_POSITION),
+        )
         gantry_move_menu.add_command(
-            label="Center X", command=lambda: self._on_event(MenuItemEnum.CENTER_X))
+            label="Center X",
+            command=lambda: self.event_bus.publish(EventEnum.CENTER_X),
+        )
         gantry_move_menu.add_command(
-            label="Center Y", command=lambda: self._on_event(MenuItemEnum.CENTER_Y))
+            label="Center Y",
+            command=lambda: self.event_bus.publish(EventEnum.CENTER_Y),
+        )
         gantry_move_menu.add_command(
-            label="Center XY", command=lambda: self._on_event(MenuItemEnum.CENTER_XY))
+            label="Center XY",
+            command=lambda: self.event_bus.publish(EventEnum.CENTER_XY),
+        )
 
     def _create_gantry_home_menu(self) -> None:
-
-        gantry_home_menu: tk.Menu = tk.Menu(self._gantry_parent_item, tearoff=0)
+        gantry_home_menu: tk.Menu = tk.Menu(
+            self._gantry_parent_item, tearoff=0
+        )
         self._gantry_parent_item.add_cascade(
-            label="Home", menu=gantry_home_menu)
+            label="Home", menu=gantry_home_menu
+        )
         gantry_home_menu.add_command(
-            label="Home X", command=lambda: self._on_event(MenuItemEnum.HOME_X))
+            label="Home X",
+            command=lambda: self.event_bus.publish(EventEnum.HOME_X),
+        )
         gantry_home_menu.add_command(
-            label="Home Y", command=lambda: self._on_event(MenuItemEnum.HOME_Y))
+            label="Home Y",
+            command=lambda: self.event_bus.publish(EventEnum.HOME_Y),
+        )
         gantry_home_menu.add_command(
-            label="Home Z", command=lambda: self._on_event(MenuItemEnum.HOME_Z))
+            label="Home Z",
+            command=lambda: self.event_bus.publish(EventEnum.HOME_Z),
+        )
         gantry_home_menu.add_command(
-            label="Home XYZ", command=lambda: self._on_event(MenuItemEnum.HOME_XYZ))
+            label="Home XYZ",
+            command=lambda: self.event_bus.publish(EventEnum.HOME_XYZ),
+        )
 
     def _create_gantry_temp_menu(self) -> None:
-        gantry_temp_menu: tk.Menu = tk.Menu(self._gantry_parent_item, tearoff=0)
+        gantry_temp_menu: tk.Menu = tk.Menu(
+            self._gantry_parent_item, tearoff=0
+        )
         self._gantry_parent_item.add_cascade(
-            label="Temperature", menu=gantry_temp_menu)
+            label="Temperature", menu=gantry_temp_menu
+        )
         gantry_temp_menu.add_command(
-            label="Set 100", command=lambda: self._on_event(MenuItemEnum.T_100))
+            label="Set 100",
+            command=lambda: self.event_bus.publish(EventEnum.T_100),
+        )
         gantry_temp_menu.add_command(
-            label="Set 75", command=lambda: self._on_event(MenuItemEnum.T_75))
+            label="Set 75",
+            command=lambda: self.event_bus.publish(EventEnum.T_75),
+        )
         gantry_temp_menu.add_command(
-            label="Set 50", command=lambda: self._on_event(MenuItemEnum.T_50))
+            label="Set 50",
+            command=lambda: self.event_bus.publish(EventEnum.T_50),
+        )
         gantry_temp_menu.add_command(
-            label="Set 25", command=lambda: self._on_event(MenuItemEnum.T_25))
+            label="Set 25",
+            command=lambda: self.event_bus.publish(EventEnum.T_25),
+        )
         gantry_temp_menu.add_command(
-            label="Set Off", command=lambda: self._on_event(MenuItemEnum.T_0))
+            label="Set Off",
+            command=lambda: self.event_bus.publish(EventEnum.T_0),
+        )
 
     def _create_settings_menu(self) -> None:
-        self.add_cascade(label='Settings', menu=self._settings_parent_item)
+        self.add_cascade(label="Settings", menu=self._settings_parent_item)
         self._settings_parent_item.add_command(
-            label='Open settings', command=lambda: self._on_event(MenuItemEnum.OPEN_SETTINGS))
+            label="Open settings",
+            command=lambda: self.event_bus.publish(EventEnum.OPEN_SETTINGS),
+        )
--- src/util/event_type_enum.py
+++ src/util/event_type_enum.py
@@ -3,3 +3,20 @@ from enum import Enum, auto
 
 class EventEnum(Enum):
     CAMERA_CONNECTION_STATE = auto()
+
+    CASCADE = auto()
+    MOVE_Y_220 = auto()
+    RESET_POSITION = auto()
+    CENTER_X = auto()
+    CENTER_Y = auto()
+    CENTER_XY = auto()
+    HOME_X = auto()
+    HOME_Y = auto()
+    HOME_Z = auto()
+    HOME_XYZ = auto()
+    T_100 = auto()
+    T_75 = auto()
+    T_50 = auto()
+    T_25 = auto()
+    T_0 = auto()
+    OPEN_SETTINGS = auto()
